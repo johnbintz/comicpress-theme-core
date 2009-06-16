8a9bbb09ac2a178b2356ef794fe72c4862c46769
John Bintz <john@coswellproductions.com>
2009-06-15 22:41:18 -0400

    Fix for FS#52

--- links.php
+++ links.php
@@ -9,15 +9,15 @@ Template Name: Links
 <div id="column">
 
 	<div class="post-page-head">
-	<div class="post-page">
-		<h2 class="pagetitle"><?php the_title() ?></h2>
-		<ul>
-			<?php wp_list_bookmarks() ?>
-		</ul>
-		<br class="clear-margins" />
-	</div>
-	<div class="post-page-foot"></div>
-	
+		<div class="post-page">
+			<h2 class="pagetitle"><?php the_title() ?></h2>
+			<ul>
+				<?php wp_list_bookmarks() ?>
+			</ul>
+			<br class="clear-margins" />
+		</div>
+		<div class="post-page-foot"></div>
+  </div>
 </div>
 	
 <?php include(TEMPLATEPATH . '/sidebar.php') ?>
