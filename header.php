54d1f78f33500ea251c1331e3ca39ffe3aa5ac15
John Bintz <john@coswellproductions.com>
2009-06-30 19:49:21 -0400

    implement FS#73

--- header.php
+++ header.php
@@ -2,7 +2,20 @@
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes() ?>>
 
 <head profile="http://gmpg.org/xfn/11">
-	<title><?php bloginfo('name') ?> <?php if ( is_single() ) { ?> &raquo; Archive <?php } wp_title() ?></title>
+	<title><?php 
+    bloginfo('name'); 
+    if (is_home () ) {
+      echo " - "; bloginfo('description');
+    } elseif (is_category() ) {
+      echo " - "; single_cat_title();
+    } elseif (is_single() || is_page() ) { 
+      echo " - "; single_post_title();
+    } elseif (is_search() ) { 
+      echo " search results: "; echo wp_specialchars($s);
+    } else { 
+      echo " - "; wp_title('',true);
+    }
+  ?></title>
 	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type') ?>; charset=<?php bloginfo('charset') ?>" />
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url') ?>" type="text/css" media="screen" />
 	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name') ?> RSS Feed" href="<?php bloginfo('rss2_url') ?>" />
