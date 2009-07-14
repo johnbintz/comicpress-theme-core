21d163cf9eefe67f49378f031769ef33355d9646
John Bintz <jcoswell@coswellproductions.org>
2009-07-14 12:30:35 -0400

    add return to top

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 <div id="footer">
 	<p>
 		<?php bloginfo('name') ?> is powered by <a href="http://wordpress.org/">WordPress</a> with <a href="http://comicpress.org/">ComicPress</a>
-		| Subscribe: <a href="<?php bloginfo('rss2_url') ?>">RSS Feed</a>
+		| Subscribe: <a href="<?php bloginfo('rss2_url') ?>">RSS Feed</a> | <a href="#outside" onclick="scrollup(); return false;">Return to Top &nbsp;</a>
 		<!-- <?php echo get_num_queries() ?> queries. <?php timer_stop(1) ?> seconds. -->
 	</p>
 </div>
