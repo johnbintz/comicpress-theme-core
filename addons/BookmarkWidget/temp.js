69ed6e95b7f8e817ab34ae10dd2b48d480e849bb
John Bintz <john@coswellproductions.com>
2009-07-14 22:24:31 -0400

    get bookmark widget working

--- addons/BookmarkWidget/temp.js
+++ addons/BookmarkWidget/temp.js
@@ -11,8 +11,8 @@
         var imgClearOff = '<?php bloginfo('template_directory'); ?>/images/3a.gif'; //no comic tagged, clear not possible
         var imgGotoOff  = '<?php bloginfo('template_directory'); ?>/images/2a.gif'; //no comic tagged, goto not possible
         var imgClearOn  = '<?php bloginfo('template_directory'); ?>/images/3.gif';  //clear a tag, shows when comic previously tagged
-        var imgGotoOn   = '<?php bloginfo('template_directory'); ?>/2.gif';  //shows when a comic is tagged  
-        var imgInfo     = '<?php bloginfo('template_directory'); ?>/4.gif';  //img that displays the help
+        var imgGotoOn   = '<?php bloginfo('template_directory'); ?>/images/2.gif';  //shows when a comic is tagged  
+        var imgInfo     = '<?php bloginfo('template_directory'); ?>/images/4.gif';  //img that displays the help
         var comicDir    = '/';                                               //alter this if you run multiple comics in different directories on your site.
 
         /* Now write out the applicable links */
