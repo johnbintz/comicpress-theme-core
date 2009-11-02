ed8231a193aa55a51b6ffc70a58d86d74e17a027
John Bintz <john@coswellproductions.com>
2009-11-01 20:20:40 -0500

    working on enhancements

---
+++ js/MediaUploader.js
@@ -0,0 +1,12 @@
+jQuery(function() {
+jQuery('.media-item').each(function() {
+	var item = this;
+	jQuery('.savesend', item).each(function() {
+		
+	});
+	jQuery('input[name*=comic_image_type]', item).bind('change', function() {
+		
+	});
+});
+
+});
